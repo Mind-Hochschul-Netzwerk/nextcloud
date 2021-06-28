c44c455fe40c23584ecb5c6f3f1f1f9888cf392a
Joshua Kreuder <Joshua_Kreuder@outlook.de>
2021-06-28 22:12:39 +0200

    Convention

--- ensurevolumes.sh
+++ ensurevolumes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env
-mkdir -p ${DATA_DIR}/app/
-mkdir -p ${DATA_DIR}/postgres/
-sudo chown -R www-data:www-data ${DATA_DIR}/app/
+mkdir -p ${VOLUMES_DIR}/app/
+mkdir -p ${VOLUMES_DIR}/postgres/
+sudo chown -R www-data:www-data ${VOLUMES_DIR}/app/
